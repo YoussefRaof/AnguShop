7d72b96923814363b234d871b7dc8178b6ec6408
Ofaa85 <youssefroaf94@gmail.com>
2025-05-03 01:09:04 +0300

    Project Architecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ProductsComponent } from "./components/products/products.component";
+import { ProductDetailsComponent } from "./components/product-details/product-details.component";
+import { CartComponent } from "./components/cart/cart.component";
+import { HomeComponent } from './components/home/home.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsComponent, ProductDetailsComponent, CartComponent,HomeComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home",component:HomeComponent,title:"Home"},
+    {path:"about",component:AboutComponent,title:"About"},
+    {path:"products",component:ProductsComponent,title:"Products" },
+    {path:"cart",component:CartComponent,title:"Cart"},
+    {path:"",redirectTo:"home",pathMatch:"full"},
+    {path:"**",redirectTo:"home",title:"Home"}
+];
