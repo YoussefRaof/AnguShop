a651f18b6793dc9b16d451a80187bf5eb2c91381
dev-muatef <mo7amed.3atef.jalil@gmail.com>
2025-05-05 05:27:32 +0300

    Refactor project Arch and re-handling routing

---
+++ src/app/Layout/main-layout/main-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../../components/header/header.component";
+import { FooterComponent } from "../../components/footer/footer.component";
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [HeaderComponent , RouterOutlet, FooterComponent],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.css'
+})
+export class MainLayoutComponent {
+
+}
