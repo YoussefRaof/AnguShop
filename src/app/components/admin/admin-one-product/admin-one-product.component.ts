2e5e80ae2ec37166f1167513899f277502d19695
Ofaa85 <youssefroaf94@gmail.com>
2025-05-14 21:21:44 +0300

    Fixing Delete In Admin

--- src/app/components/admin/admin-one-product/admin-one-product.component.ts
+++ src/app/components/admin/admin-one-product/admin-one-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule, NgClass, SlicePipe } from '@angular/common';
 import { Product } from '../../../interfaces/product';
+import { ProductsService } from '../../../../Services/products.service';
 
 @Component({
   selector: 'app-admin-one-product',
@@ -12,6 +13,9 @@ import { Product } from '../../../interfaces/product';
 export class AdminOneProductComponent {
   @Input() product!: Product;
 
+  constructor(private productService:ProductsService){
+
+  }
   @Output() delete = new EventEmitter<number>();
   @Output() viewDetails = new EventEmitter<number>();
   @Output() edit = new EventEmitter<number>();
@@ -20,9 +24,27 @@ export class AdminOneProductComponent {
     return Math.floor(this.product.rating?.rate || 0);
   }
 
-  onDelete(): void {
+  // onDelete(): void {
+  //   this.delete.emit(this.product.id);
+  // }
+onDelete(): void {
+  const localProducts = JSON.parse(localStorage.getItem('addedProducts') || '[]');
+
+  // Check if the product exists in localStorage
+  const index = localProducts.findIndex((p: any) => p.id === this.product.id);
+
+  if (index !== -1) {
+    // Product found in localStorage, delete it from there
+    localProducts.splice(index, 1);
+    localStorage.setItem('addedProducts', JSON.stringify(localProducts));
+    // alert('Product deleted from local storage!');
+    this.delete.emit(this.product.id);
+  } else {
+   
     this.delete.emit(this.product.id);
   }
+}
+
 
   onView(): void {
     this.viewDetails.emit(this.product.id);
